bbfa40a9aad2aebe570f6e70c230e1436d96545a
royal4j <rufi_a@hotmail.com>
2016-05-12 16:02:41 +0100

    Added functionality for promotions  - Oranges and apple

---
+++ src/main/java/com/cg/shopping/cart/offers/NoPromotion.java
@@ -0,0 +1,15 @@
+package com.cg.shopping.cart.offers;
+
+import com.cg.shopping.cart.Product;
+
+/**
+ * Created by Rufi on 5/12/2016.
+ */
+public class NoPromotion implements Promotion {
+
+
+    @Override
+    public void applyPromotion(Product product) {
+
+    }
+}
--- src/test/java/com/cg/tests/ShoppingCartTest.java
+++ src/test/java/com/cg/tests/ShoppingCartTest.java
@@ -48,4 +48,36 @@ public class ShoppingCartTest {
         Assert.assertEquals(1, cart.getTotalProducts());
         Assert.assertEquals(1.8, cart.getTotalAmount(),0.0);
     }
+
+    @Test
+    public void testAddOrangeAndApplyOfferToCart() {
+        //I've used the same class for both solutions for buy one get 1 free and 2 for 1, perhaps we can change the class name
+        Promotion twoFor1 = new BuyOneGetOneFree(2,1);
+        ShoppingCart cart = new ShoppingCart();
+        cart.setPromotion(twoFor1);
+        Product oranges = new Product("Orange", 3, 0.75);
+        cart.addProduct(oranges);
+        Assert.assertEquals(1, cart.getTotalProducts());
+        Assert.assertEquals(0.5, cart.getTotalAmount(),0.0);
+    }
+
+    @Test
+    public void testAddAppleAndOrangeAndAppleOfferToCart() {
+        Promotion offer = new BuyOneGetOneFree(1,1);
+        Promotion twoForOne = new BuyOneGetOneFree(2,1);
+        ShoppingCart cart = new ShoppingCart();
+
+        //Add Apple and apply promotion for apple
+        Product product = new Product("Apple", 5,3.0 );
+        cart.setPromotion(offer);
+        cart.addProduct(product);
+
+        //Add Oranges and apply promotion for oranges
+        Product oranges = new Product("Orange", 3, 0.75);
+        cart.setPromotion(twoForOne);
+        cart.addProduct(oranges);
+
+        Assert.assertEquals(2, cart.getTotalProducts());
+        Assert.assertEquals(2.30, cart.getTotalAmount(),0.0);
+    }
 }
